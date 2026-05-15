a367d5032c0683116dce29cb428df567edaaa1d1
Alex Tsarkov <alex.tsarkov@gmail.com>
2026-05-15 14:33:32 +0300

    feat: add news search live component

---
+++ src/Twig/Components/News/Search.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Twig\Components\News;
+
+use App\Entity\News;
+use App\Repository\NewsRepository;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+#[AsLiveComponent]
+final class Search
+{
+    use DefaultActionTrait;
+
+    #[LiveProp(writable: true)]
+    public string $query = '';
+
+    #[LiveProp]
+    public ?int $limit = null;
+
+    public function __construct(
+        private NewsRepository $newsRepository,
+    ) {}
+
+    /**
+     * @return News[]
+     */
+    public function getNews(): array
+    {
+        return $this->newsRepository->findByQuery($this->query, $this->limit);
+    }
+}
